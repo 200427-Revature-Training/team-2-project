721024f4668277e335176e9ad551f5d6f95ff679
Evan <ELiebhauser@gmail.com>
2020-06-24 06:33:52 -0700

    login component

--- client/src/remote/admin.remote.ts
+++ client/src/remote/admin.remote.ts
@@ -1,12 +1,12 @@
 import { internalAxios, authAxios } from './internal-axios';
-import { Administrator } from '../models/Administrators';
+import { Administrator } from '../models/Admin-All-Tickets';
 
 
 // Get all tickets table
 export const getAllTicketTable = async () => {
     const response = await internalAxios.get<Administrator[]>('/employees/reimburse');
     return response.data.map(reimburse => {
-        reimburse.sumitDate = new Date(reimburse.sumitDate); // Replace string birthdate with Date object
+        // reimburse.sumitDate = new Date(reimburse.sumitDate); // Replace string birthdate with Date object
         reimburse.resolvedDate = new Date(reimburse.resolvedDate);
         console.log(response);
         return reimburse;
